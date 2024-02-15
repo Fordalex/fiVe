d75fd4088fd6557e3b1edacbcb345cb2640b0da8
Fordalex <alex96ford19@gmail.com>
2024-02-15 13:16:50 +0000

    Add buttons to create people

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,73 +1,156 @@
-class Person {
-    constructor(x, y, direction) {
-        this.x = x;
-        this.y = y;
-        this.width = 100;
-        this.direction = direction;
-        this.people = people;
-        this.health = 100;
-        this.state = 'walking';
-        this.createPerson();
-        this.people.push(this);
+document.addEventListener('DOMContentLoaded', () => {
+    const createButtons = document.querySelectorAll('[data-create]');
+    const gameContainer = document.getElementById('gameContainer');
+    const leftStats = {
+        health: 100,
+        person: {
+            damage: 10,
+        }
     }
-    
-    createPerson() {
-        var person = document.createElement('div');
-        person.style.position = 'absolute';
-        person.style.left = `${this.x}px`;
-        person.classList.add(`person-walking-${this.direction}`);
-        document.body.appendChild(person);
-        this.person = person;
+    const rightStats = {
+        health: 100,
+        person: {
+            damage: 10,
+        }
     }
 
-    update() {
-        if (this.canMove()) {
+    createButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            var direction = button.dataset.direction;
+            var damage = eval(`${direction}Stats.person.damage`);
+            new Person(direction, damage);
+        });
+    });
+
+    function updateStats() {
+        document.getElementById('leftHealth').innerText = leftStats.health;
+        document.getElementById('rightHealth').innerText = rightStats.health;
+        document.getElementById('leftPersonStats').innerText = `Damage: ${leftStats.person.damage}`;
+        document.getElementById('rightPersonStats').innerText = `Damage: ${rightStats.person.damage}`;
+    }
+
+    updateStats();
+
+    class Person {
+        constructor(direction, damage) {
+            this.x = null;
+            this.y = 0;
+            this.width = 100;
+            this.height = 100;
+            this.damage = damage;
+
+            this.direction = direction;
+            this.people = people;
             this.state = 'walking';
-        } else {
-            this.state = 'attacking';
+
+            this.health = 100;
+            this.healthBar = null;
+            
+            this.createPerson();
+            this.createHealthBar();
+            this.people.push(this);
+        }
+        
+        createPerson() {
+            this.x = this.direction === 'left' ? 1000 : 0;
+
+            var person = document.createElement('div');
+            person.style.position = 'absolute';
+            person.style.left = `${this.x}px`;
+            person.classList.add(`person-walking-${this.direction}`);
+            gameContainer.appendChild(person);
+            this.person = person;
+            this.personInFrontOfYou = null;
         }
 
-        if (this.state === 'walking') {
-            this.moveForward();
-        } else if (this.state === 'attacking') {
-            this.attack();
+        createHealthBar() {
+            var healthBar = document.createElement('div');
+            healthBar.style.width = `${this.health}px`;
+            healthBar.style.top = `${this.height + 10}px`;
+            healthBar.style.left = `${this.x}px`;
+            healthBar.classList.add('health-bar');
+            gameContainer.appendChild(healthBar);
+            this.healthBar = healthBar;
         }
-    }
-    
-    moveForward() {
-        this.x += this.walkingSpeed();
-        this.person.style.left = `${this.x}px`;
-    }
 
-    attack() {
-        this.person.classList.remove(`person-walking-${this.direction}`);
-        this.person.classList.add(`person-attacking-${this.direction}`);
-    }
-    
-    walkingSpeed() {
-        return this.direction === 'left' ? -15 : 15;
-    }
+        update() {
+            if (this.canMove()) {
+                this.state = 'walking';
+            } else {
+                this.state = 'attacking';
+            }
 
-    canMove() {
-        for (let otherPerson of this.people) {
-            if (otherPerson !== this) {
-                // Assuming a simple rule where they stop if they are less than 20px apart
-                if (Math.abs(this.x - otherPerson.x) <= this.width - 45) {
-                    return false; // Too close to another person, cannot move
-                }
+            this.setCurrentAnimation();
+
+            if (this.state === 'walking') {
+                this.moveForward();
+            } else if (this.state === 'attacking') {
+                this.attack();
             }
         }
-        return true; // No one is close, can move
+
+        setCurrentAnimation() {
+            this.person.classList.remove(`person-walking-${this.direction}`);
+            this.person.classList.remove(`person-attacking-${this.direction}`);
+
+            if (this.state === 'walking') {
+                this.person.classList.add(`person-walking-${this.direction}`);
+            }
+
+            if (this.state === 'attacking') {
+                this.person.classList.add(`person-attacking-${this.direction}`);
+            }
+        }
+        
+        moveForward() {
+            this.x += this.walkingSpeed();
+            this.person.style.left = `${this.x}px`;
+            this.healthBar.style.left = `${this.x}px`;
+        }
+
+        attack() {
+            if (this.personInFrontOfYou) {
+                this.personInFrontOfYou.attacked(this.damage);
+            }
+        }
+
+        attacked(damage) {
+            this.health -= damage;
+            this.healthBar.style.width = `${this.health}px`;
+
+            if (this.health <= 0) {
+                this.die();
+            }
+        }
+        
+        walkingSpeed() {
+            return this.direction === 'left' ? -15 : 15;
+        }
+
+        die() {
+            this.person.remove();
+            this.healthBar.remove();
+            this.people.splice(this.people.indexOf(this), 1);
+        }
+
+        canMove() {
+            for (let otherPerson of this.people) {
+                if (otherPerson !== this) {
+                    if (Math.abs(this.x - otherPerson.x) <= this.width - 45) {
+                        this.personInFrontOfYou = otherPerson;
+                        return false;
+                    }
+                }
+            }
+            return true; // No one is close, can move
+        }
     }
-}
 
-var people = [];
+    var people = [];
 
-var person = new Person(0, 0, 'right', people);
-var person2 = new Person(400, 0, 'left', people);
-
-setInterval(function () {
-    people.forEach(person => {
-        person.update();
-    });
-}, 200);
+    setInterval(function () {
+        people.forEach(person => {
+            person.update();
+        });
+    }, 200);
+});
